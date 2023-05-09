88b09fc3962c83623d963f7436a5939737024a50
Wladimir Rocha <wldrocha@gmail.com>
2023-05-09 16:59:43 -0400

    [add] layouts and files

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import Nav from 'common/Nav'
+import Header from 'components/Header'
+
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <div className="min-h-full">
+        <Header />
+        <Nav />
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default MainLayout
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@styles/tailwind.css'
+import MainLayout from 'layout/MainLayout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  )
 }
