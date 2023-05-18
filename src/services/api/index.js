bfa4ce7e93dcac9a2822e479e2e791a50a4de5ff
Wladimir Rocha <wldrocha@gmail.com>
2023-05-18 10:50:02 -0400

    [ADD] endpoints

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,11 +1,29 @@
 const API = process.env.NEXT_PUBLIC_API_URL
 
 export const endPoints = {
+  products: {
+    getProducts: `${API}/api/v1/products/`,
+    postProducts: `${API}/api/v1/products/`,
+    getProduct: (id) => `${API}/api/v1/products/${id}`,
+    putProducts: (id) => `${API}/api/v1/products/${id}`,
+    deleteProducts: (id) => `${API}/api/v1/products/${id}`,
+  },
+  users: {
+    getUsers: `${API}/api/v1/users`,
+    postUsers: `${API}/api/v1/users`,
+  },
   auth: {
     login: `${API}/api/v1/auth/login`,
     profile: `${API}/api/v1/auth/profile`,
   },
-  products: {
-    getProduct: (id) => `${API}/api/v1/product/${id}`,
+  categories: {
+    getCategories: `${API}/api/v1/categories`,
+    postCategories: `${API}/api/v1/categories`,
+    getCategoriesProduct: (id) => `${API}/api/v1/categories/${id}/products`,
+    putCategories: (id) => `${API}/api/v1/categories/${id}`,
+  },
+  files: {
+    postFiles: `${API}/api/v1/files/upload`,
+    getFiles: (fileName) => `${API}/api/v1/${fileName}`,
   },
 }
